ac771a82caa4e226b76fc7920becbcefaa4f8a65
AndyStar <andy_star@ukr.net>
2025-05-29 13:27:37 +0300

    First FastAPI, Flake8 noticed an issue - fixed

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -37,7 +37,8 @@ class MovieListResponseSchema(BaseModel):
                             "date": "2023-03-02",
                             "score": 73,
                             "genre": "Drama,Action",
-                            "overview": "After dominating the boxing world, Adonis Creed has been thriving in both his career and family life...",
+                            "overview": "After dominating the boxing world, Adonis Creed "
+                                        "has been thriving in both his career and family life...",
                             "crew": "Michael B. Jordan, Tessa Thompson",
                             "orig_title": "Creed III",
                             "status": "Released",
